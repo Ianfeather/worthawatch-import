f334a67efe9aa1f8d19b3988f9530a017aec9f74
Ian Feather <info@ianfeather.co.uk>
2018-09-04 10:41:17 +0100

    Working with erikberg API

--- handler.js
+++ handler.js
@@ -1,11 +1,15 @@
 let getData = require('./lib/get-data');
 
 module.exports.import = async (event, context) => {
-
-  let body = JSON.stringify(getData(event))
-
-  return {
-    statusCode: 200,
-    body,
-  };
+  try {
+    return {
+      statusCode: 200,
+      body: await getData(event)
+    };
+  } catch (e) {
+    return {
+      statusCode: e.status,
+      body: e.statusText
+    }
+  }
 };
