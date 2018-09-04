456843d902a888a637a70df9ecf826afdc86c02d
Ian Feather <info@ianfeather.co.uk>
2018-09-05 00:16:06 +0100

    Structure

--- handler.js
+++ handler.js
@@ -2,14 +2,9 @@ let getData = require('./lib/get-data')
 
 module.exports.import = async (event, context) => {
   try {
-    return {
-      statusCode: 200,
-      body: await getData(event)
-    }
+    return { statusCode: 200, body: await getData(event) }
   } catch (e) {
-    return {
-      statusCode: e.status,
-      body: e.statusText
-    }
+    // This might need improving. Right now it will return any upstream status
+    return { statusCode: e.status, body: e.statusText }
   }
 }
