f7c38b4db222f1e9421daa43ade654d896ed7818
Ian Feather <info@ianfeather.co.uk>
2018-09-04 23:35:53 +0100

    Working

--- handler.js
+++ handler.js
@@ -1,15 +1,15 @@
-let getData = require('./lib/get-data');
+let getData = require('./lib/get-data')
 
 module.exports.import = async (event, context) => {
   try {
     return {
       statusCode: 200,
       body: await getData(event)
-    };
+    }
   } catch (e) {
     return {
       statusCode: e.status,
       body: e.statusText
     }
   }
-};
+}
