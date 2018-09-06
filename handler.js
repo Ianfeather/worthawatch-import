e82b7efa74be418837e0786b008b43c70b616a7b
Ian Feather <info@ianfeather.co.uk>
2018-09-06 09:08:45 +0100

    Set up cron to check token expiry and email me

--- handler.js
+++ handler.js
@@ -1,10 +1,20 @@
 let getData = require('./lib/get-data')
+let checkTokenExpiry = require('./lib/check-token-expiry')
 
 module.exports.import = async (event, context) => {
   try {
     return { statusCode: 200, body: await getData(event) }
   } catch (e) {
     // This might need improving. Right now it will return any upstream status
+    // which could be erikberg or the db
+    return { statusCode: e.status, body: e.statusText }
+  }
+}
+
+module.exports.token = async (event, context) => {
+  try {
+    return { statusCode: 200, body: await checkTokenExpiry(event) }
+  } catch (e) {
     return { statusCode: e.status, body: e.statusText }
   }
 }
